caf68f6a2134c0703c05ba77362d97e4115d20d9
unknown <kellynamaral@yahoo.com>
2023-06-27 14:01:03 -0300

    implantação do store

--- src/components/temas/cadastroTema/CadastroTema.tsx
+++ src/components/temas/cadastroTema/CadastroTema.tsx
@@ -5,79 +5,86 @@ import {useNavigate, useParams } from 'react-router-dom';
 import { buscaId, post, put } from '../../../services/Service';
 import useLocalStorage from 'react-use-localstorage';
 import './CadastroTema.css';
+import { useSelector } from 'react-redux';
+import { TokenState } from '../../../store/tokens/tokensReducer';
 
 
 
 function CadastroTema() {
     let navigate = useNavigate();
-    const { id } = useParams<{ id: string }>();
-    const [token, setToken] = useLocalStorage('token');
-    const [tema, setTema] = useState <Tema>({
+    const { id } = useParams<{id: string}>();
+    const token = useSelector<TokenState, TokenState["tokens"]>(
+        (state) => state.tokens
+      );
+    const [tema, setTema] = useState<Tema>({
         id: 0,
         descricao: ''
     })
 
     useEffect(() => {
-        if (token == '') {
-            alert('Você precisa estar logado')
-            navigate('/login')
+        if (token == "") {
+            alert("Você precisa estar logado")
+            navigate("/login")
+    
         }
     }, [token])
 
-    useEffect(() => {
-        if (id !== undefined) {
+    useEffect(() =>{
+        if(id !== undefined){
             findById(id)
         }
     }, [id])
 
     async function findById(id: string) {
-        buscaId('/tema/${id}', setTema, {
+        buscaId(`/tema/${id}`, setTema, {
             headers: {
-               ' Authorization': token
+              'Authorization': token
             }
-        })
-    }
-
-    function updatedTema(e: ChangeEvent<HTMLInputElement>) {
-
-        setTema({
-            ...tema,
-            [e.target.name]: e.target.value,
-        })
-    }
-
-    async function onSubmit(e: ChangeEvent<HTMLFormElement>) {
-        e.preventDefault()
-        console.log('tema' + JSON.stringify(tema))
-
-        if (id !== undefined) {
-            console.log(tema)
-            put('./tema', tema, setTema, {
-                headers: {
-                    'Authorization': token
-                }
-            })
-            alert('Tema atualizado com sucesso');
-        } else {
-            post('./tema', tema, setTema, {
-                headers: {
-                    'Authorization': token
-                }
-            })
-            alert('Tema cadastrado com sucesso');
+          })
         }
-        back()
-    }
 
-    function back(){
-        navigate('temas')
-    }
+        function updatedTema(e: ChangeEvent<HTMLInputElement>) {
 
+            setTema({
+                ...tema,
+                [e.target.name]: e.target.value,
+            })
+    
+        }
+        
+        async function onSubmit(e: ChangeEvent<HTMLFormElement>) {
+            e.preventDefault()
+            console.log("tema " + JSON.stringify(tema))
+    
+            if (id !== undefined) {
+                console.log(tema)
+                put(`/tema`, tema, setTema, {
+                    headers: {
+                        'Authorization': token
+                    }
+                })
+                alert('Tema atualizado com sucesso');
+            } else {
+                post(`/tema`, tema, setTema, {
+                    headers: {
+                        'Authorization': token
+                    }
+                })
+                alert('Tema cadastrado com sucesso');
+            }
+            back()
+    
+        }
+    
+        function back() {
+            navigate('/temas')
+        }
+  
     return (
         <Container maxWidth="sm" className="topo">
             <form onSubmit={onSubmit}>
                 <Typography variant="h3" color="textSecondary" component="h1" align="center" >Formulário de cadastro tema</Typography>
-                <TextField  value={tema,descricao} onChange={(e: ChangeEvent<HTMLInputElement>) => updatedTema(e)} id="descricao" label="descricao" variant="outlined" name="descricao" margin="normal" fullWidth />
+                <TextField value={tema.descricao} onChange={(e: ChangeEvent<HTMLInputElement>) => updatedTema(e)} id="descricao" label="descricao" variant="outlined" name="descricao" margin="normal" fullWidth />
                 <Button type="submit" variant="contained" color="primary">
                     Finalizar
                 </Button>
