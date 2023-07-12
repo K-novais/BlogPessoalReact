931486fa8b8f53c187ba994276ac2fa0da48bf3d
Kelly  Amaral <106094067+K-novais@users.noreply.github.com>
2023-07-12 13:03:07 -0300

    Update modalPostagem.tsx

--- src/components/postagens/modalPostagem/modalPostagem.tsx
+++ src/components/postagens/modalPostagem/modalPostagem.tsx
@@ -4,7 +4,7 @@ import Modal from '@material-ui/core/Modal';
 import {Button } from "@material-ui/core"
 import {Box} from '@mui/material';
 import CloseIcon from '@material-ui/icons/Close';
-import './ModalPostagem.css';
+import './modalPostagem.css';
 import CadastroPost from '../cadastroPost/CadastroPost';
 
 
